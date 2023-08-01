d9c777f5192bd97da88469e476da43edea69e03e
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-08-01 01:37:46 -0300

    setup project + add getStaticProps to set products and render items from build time

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+export default function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return { props: { products: [{ id: 'p1', title: 'Product 1' }] } };
+}
