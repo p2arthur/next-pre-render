412ee57cb2238de168df2be2acdb9f972e2bd11c
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-08-02 21:06:19 -0300

    add redirect and not found properties to the getStaticProps on index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,35 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 export default function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li>{product.title}</li>
+        <li key={product.id}>{product.title}</li>
       ))}
     </ul>
   );
 }
 
-export async function getStaticProps() {
-  return { props: { products: [{ id: 'p1', title: 'Product 1' }] } };
+export async function getStaticProps(context) {
+  console.log('getStaticProps context', context);
+  console.log('regenerating');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const { products } = JSON.parse(jsonData);
+
+  if (!jsonData) {
+    return { redirect: { detination: '/no-data' } };
+  }
+
+  if (products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { products: products },
+    revalidate: 5,
+  };
 }
